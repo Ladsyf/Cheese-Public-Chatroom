547cf6edf32028c366c5633f2d15ef40121ab1a8
almar <almarjavier24@gmail.com>
2022-08-01 20:46:51 +0800

    converting prototype into working system p1

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask, request, render_template, flash, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
@@ -12,12 +14,19 @@ app.config['SECRET_KEY'] = '*$0)rdca5#fNJLFfF]3E'
 
 db = SQLAlchemy(app)
 
+
 class Rooms(db.Model):
     RID = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    visibility = db.Column(db.String(20), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    messages = db.Column(db.Integer)
 
-    def __init__(self, name):
+    def __init__(self, name, date, visibility, messages):
         self.name = name
+        self.visibility = visibility
+        self.date = date
+        self.messages = messages
 
     def __repr__(self):
         return '<Rooms %r>' % self.RID
@@ -41,9 +50,36 @@ db.session.commit()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == "POST":
-        CRUDroom.add(db, Rooms, "HelloGuys")
+    public_room = Rooms.query.filter_by(visibility = "public").all()
+    return render_template('listrooms.html', rooms = public_room)
 
-    rooms = Rooms.query.all()
-    return render_template('listrooms.html', rooms = rooms)
+@app.route('/createroom', methods=['GET', 'POST'])
+def createroom():
+    if request.method == "POST":
+        if not request.form['name']:
+            flash('Please Enter a Room Name!')
+        elif Rooms.query.filter_by(name=request.form['name']).first():
+            flash('Name already exists!')
+        else:
+            date = datetime.now()
+            add = Rooms(request.form['name'], date, request.form['visibility'], 0)
+
+            db.session.add(add)
+            db.session.commit()
+            flash('Successfully Added!')
+            return redirect(url_for('index'))
+    return render_template('createroom.html')
+
+@app.route('/room/<RID>/<name>', methods=['GET'])
+def roomview(RID, name):
+    room = Rooms.query.filter_by(RID=RID, name = name).first()
+    messages = Logs.query.filter_by(RID=RID).all()
+    if room:
+        return render_template('room.html', RID = RID, name = name, room = room, messages = messages)
+    else:
+        return redirect(url_for('index'))
+
+
+
+
 
